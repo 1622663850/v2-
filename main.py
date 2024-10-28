070a976e77db5db9cc29fbb68efe9ab162ef5ba9
1622663850 <663809323@qq.com>
2024-10-28 21:39:51 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,39 +17,39 @@ def encode_file(input_file, output_file):
 # 按装订区域中的绿色按钮以运行脚本。
 if __name__ == '__main__':
 
-    vmess = {
-      "v": "2",
-      "ps": "v.ps-CF优选IP-",
-      "add": "",
-      "port": "8880",
-      "id": "8fa3f184-cdb4-4b25-b985-dbb2f89b367c",
-      "aid": "0",
-      "scy": "auto",
-      "net": "ws",
-      "type": "none",
-      "host": "vs2.xqzy.cloudns.org",
-      "path": "/ray2",
-      "tls": "",
-      "sni": "",
-      "alpn": ""
-    }
+    # vmess = {
+    #   "v": "2",
+    #   "ps": "v.ps-CF优选IP-",
+    #   "add": "",
+    #   "port": "8880",
+    #   "id": "8fa3f184-cdb4-4b25-b985-dbb2f89b367c",
+    #   "aid": "0",
+    #   "scy": "auto",
+    #   "net": "ws",
+    #   "type": "none",
+    #   "host": "vs2.xqzy.cloudns.org",
+    #   "path": "/ray2",
+    #   "tls": "",
+    #   "sni": "",
+    #   "alpn": ""
+    # }
 
 
     # 示例用法
     input_file = 'input.txt'
 
     # 发送请求获取 IP 地址
-    response = requests.get('https://monitor.gacjie.cn/api/client/get_ip_address?cdn_server=1')#CDN运营商CloudFlare=1 CloudFront=2 Gcore=3
+    response = requests.get('https://www.wetest.vip/api/cf2dns/get_cloudflare_ip')#CDN运营商CloudFlare=1 CloudFront=2 Gcore=3
     new_vmess = []
 
-    vmess_1 = vmess
-    vmess_1['ps'] = 'v.ps-CF优选IP-原地址'
-    vmess_1['add'] = 'vs2.xqzy.cloudns.org'  # 地址
-    json_str = json.dumps(vmess_1, ensure_ascii=False)
-    json_bytes = json_str.encode('utf-8')
-    encoded_content = base64.b64encode(json_bytes)
-    base64_str = encoded_content.decode('utf-8')
-    new_vmess.append("vmess://" + base64_str)
+    # vmess_1 = vmess
+    # vmess_1['ps'] = 'v.ps-CF优选IP-原地址'
+    # vmess_1['add'] = 'vs2.xqzy.cloudns.org'  # 地址
+    # json_str = json.dumps(vmess_1, ensure_ascii=False)
+    # json_bytes = json_str.encode('utf-8')
+    # encoded_content = base64.b64encode(json_bytes)
+    # base64_str = encoded_content.decode('utf-8')
+    # new_vmess.append("vmess://" + base64_str)
     new_vmess.append("vless://a65f9108-df2d-4cd7-ae0a-e3eb6407e353@172.67.74.99:80?encryption=none&security=none&sni=v2rat.xqzy.workers.dev&fp=randomized&type=ws&host=v2rat.xqzy.workers.dev&path=%2F%3Fed%3D2048#v2rat.xqzy.workers.dev")
 
     if response.status_code == 200:
@@ -62,53 +62,53 @@ if __name__ == '__main__':
             for ip_ in ip:
                 # print(ip_["ip"])
                 jishu += 1
-                vmess_1 = vmess
-                vmess_1['ps'] = 'v.ps-CF优选IP-移动'+ str(jishu) # 名称
-                vmess_1['add'] = ip_["ip"] # 地址
-                # 加密
-                json_str = json.dumps(vmess_1,ensure_ascii=False)
-                json_bytes = json_str.encode('utf-8')
-                encoded_content = base64.b64encode(json_bytes)
-                base64_str = encoded_content.decode('utf-8')
-                new_vmess.append("vmess://"+base64_str)
+                # vmess_1 = vmess
+                # vmess_1['ps'] = 'v.ps-CF优选IP-移动'+ str(jishu) # 名称
+                # vmess_1['add'] = ip_["ip"] # 地址
+                # # 加密
+                # json_str = json.dumps(vmess_1,ensure_ascii=False)
+                # json_bytes = json_str.encode('utf-8')
+                # encoded_content = base64.b64encode(json_bytes)
+                # base64_str = encoded_content.decode('utf-8')
+                # new_vmess.append("vmess://"+base64_str)
                 new_vmess.append(f"vless://a65f9108-df2d-4cd7-ae0a-e3eb6407e353@{ip_['ip']}:80?encryption=none&security=none&sni=v2rat.xqzy.workers.dev&fp=randomized&type=ws&host=v2rat.xqzy.workers.dev&path=%2F%3Fed%3D2048#v2rat.xqzy.workers.dev-CM{str(jishu)}")
-                # print(ip_["ip"] + '转换链接成功')
+                print(ip_["ip"] + '转换链接成功')
 
             jishu = 0
             ip = data['info']['CU']
             for ip_ in ip:
                 # print(ip_["ip"])
                 jishu += 1
-                vmess_1 = vmess
-                vmess_1['ps'] = 'v.ps-CF优选IP-联通' + str(jishu)  # 名称
-                vmess_1['add'] = ip_["ip"]  # 地址
-                # 加密
-                json_str = json.dumps(vmess_1, ensure_ascii=False)
-                json_bytes = json_str.encode('utf-8')
-                encoded_content = base64.b64encode(json_bytes)
-                base64_str = encoded_content.decode('utf-8')
-                new_vmess.append("vmess://" + base64_str)
+                # vmess_1 = vmess
+                # vmess_1['ps'] = 'v.ps-CF优选IP-联通' + str(jishu)  # 名称
+                # vmess_1['add'] = ip_["ip"]  # 地址
+                # # 加密
+                # json_str = json.dumps(vmess_1, ensure_ascii=False)
+                # json_bytes = json_str.encode('utf-8')
+                # encoded_content = base64.b64encode(json_bytes)
+                # base64_str = encoded_content.decode('utf-8')
+                # new_vmess.append("vmess://" + base64_str)
                 new_vmess.append(
                     f"vless://a65f9108-df2d-4cd7-ae0a-e3eb6407e353@{ip_['ip']}:80?encryption=none&security=none&sni=v2rat.xqzy.workers.dev&fp=randomized&type=ws&host=v2rat.xqzy.workers.dev&path=%2F%3Fed%3D2048#v2rat.xqzy.workers.dev-CU{str(jishu)}")
-                # print(ip_["ip"] + '转换链接成功')
+                print(ip_["ip"] + '转换链接成功')
 
             jishu = 0
             ip = data['info']['CT']
             for ip_ in ip:
                 # print(ip_["ip"])
                 jishu += 1
-                vmess_1 = vmess
-                vmess_1['ps'] = 'v.ps-CF优选IP-电信' + str(jishu)  # 名称
-                vmess_1['add'] = ip_["ip"]  # 地址
-                # 加密
-                json_str = json.dumps(vmess_1, ensure_ascii=False)
-                json_bytes = json_str.encode('utf-8')
-                encoded_content = base64.b64encode(json_bytes)
-                base64_str = encoded_content.decode('utf-8')
-                new_vmess.append("vmess://" + base64_str)
+                # vmess_1 = vmess
+                # vmess_1['ps'] = 'v.ps-CF优选IP-电信' + str(jishu)  # 名称
+                # vmess_1['add'] = ip_["ip"]  # 地址
+                # # 加密
+                # json_str = json.dumps(vmess_1, ensure_ascii=False)
+                # json_bytes = json_str.encode('utf-8')
+                # encoded_content = base64.b64encode(json_bytes)
+                # base64_str = encoded_content.decode('utf-8')
+                # new_vmess.append("vmess://" + base64_str)
                 new_vmess.append(
                     f"vless://a65f9108-df2d-4cd7-ae0a-e3eb6407e353@{ip_['ip']}:80?encryption=none&security=none&sni=v2rat.xqzy.workers.dev&fp=randomized&type=ws&host=v2rat.xqzy.workers.dev&path=%2F%3Fed%3D2048#v2rat.xqzy.workers.dev-CT{str(jishu)}")
-                # print(ip_["ip"] +'转换链接成功')
+                print(ip_["ip"] +'转换链接成功')
 
             # min_delay_ip = min(data['info']['CM'] + data['info']['CU'] + data['info']['CT'], key=lambda x: x['delay'])
             # NEW_IP = min_delay_ip['ip']
@@ -125,4 +125,5 @@ if __name__ == '__main__':
             f.write("%s\n" % item)
     output_file = 'output.txt'
     encode_file(input_file, output_file)
-    # print("写入到txt中成功")
+    print("写入到txt中成功")
+
